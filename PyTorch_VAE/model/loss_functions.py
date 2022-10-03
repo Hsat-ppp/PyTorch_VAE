7ee1b6587e9f65162276f80bb732011666ac1aa6
hayaho_sato <hsatoo10@gmail.com>
2022-10-03 11:26:44 +0900

    revised MSE loss definition

--- PyTorch_VAE/model/loss_functions.py
+++ PyTorch_VAE/model/loss_functions.py
@@ -20,7 +20,7 @@ class AELossMSE(nn.Module):
         super().__init__()
 
     def forward(self, input_data, output_data):
-        loss = torch.mean(torch.sum((input_data - output_data)**2, dim=(1, 2, 3)))
+        loss = 0.5 * torch.mean(torch.sum((input_data - output_data)**2, dim=(1, 2, 3)))
         return loss
 
 
@@ -41,7 +41,7 @@ class VAELossMSE(nn.Module):
         super().__init__()
 
     def forward(self, mean, log_var, input_data, output_data):
-        log_likelihood = -1.0 * torch.mean(torch.sum((input_data - output_data)**2, dim=(1, 2, 3)))
+        log_likelihood = -0.5 * torch.mean(torch.sum((input_data - output_data)**2, dim=(1, 2, 3)))
         minus_kld = torch.mean(0.5 * torch.sum(1 + log_var - (mean*mean) - torch.exp(log_var), dim=1))
         loss = -1.0 * (lrec*log_likelihood + lkld*minus_kld)  # ELBOから損失に変換するため，符号を反転
         return loss, -1.0 * log_likelihood, -1.0 * minus_kld
